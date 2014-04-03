d021db452074e1f12054c6268baeb45f18cbb3f0
mcguire <mcguire@MU-C3-TEACHER01.mullen.pvt.k12.co.us>
2014-04-03 13:27:22 -0600

    Initial commit

---
+++ Sort Sorter/src/SortSorter.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SortSorter
+	{
+	public static void main(String[] args)
+		{
+		int integerArray [] = {3, 7, 9, 2, 1};
+		Arrays.sort(integerArray);
+		
+		for (int sortedIntegers : integerArray)
+			{
+			System.out.println(sortedIntegers);
+			}
+		}
+	}
